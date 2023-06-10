446e03bade4943c9111660b1939f9f86cd5076fd
abj90 <abrahamcastrojc@gmail.com>
2023-06-10 16:45:25 +0200

    feat: search-bar created

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,42 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnDestroy,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { MatInput } from '@angular/material/input';
+import {
+  Subscription,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  fromEvent,
+  tap,
+} from 'rxjs';
+import { KEYUP_EVENT } from 'src/common/constants';
+
+@Component({
+  selector: 'search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+})
+export class SearchBarComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('input', { static: true }) input: ElementRef;
+  @Output() searchEvent = new EventEmitter<string>();
+  private subcription: Subscription;
+
+  ngAfterViewInit(): void {
+    this.subcription = fromEvent(this.input.nativeElement, KEYUP_EVENT)
+      .pipe(filter(Boolean), debounceTime(1000), distinctUntilChanged())
+      .subscribe({
+        next: () => this.searchEvent.next(this.input.nativeElement.value),
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subcription.unsubscribe();
+  }
+}
